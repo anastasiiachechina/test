e19fc1e7bed76bdb121fa72387369812b4c4abb8
nastya-chechyna <nastya.chechyna@gmail.com>
2017-09-13 23:58:34 +0300

    correct period

--- specs/conf.js
+++ specs/conf.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   CITY: 'New York',
-  START_DATE: '2017-12-15',
-  END_DATE: '‎2017-12-20',
+  START_DATE: '2017-12-10',
+  END_DATE: '‎2017-12-15',
   CHECK_IN_ELEM: '.--checkin-field',
   CHECK_OUT_ELEM: '.--checkout-field'
 };
